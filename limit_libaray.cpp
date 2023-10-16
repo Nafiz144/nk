9361bf26bccb3f31410b379aacc50d2607f4d6a4
Nafiz <125043854+Nafiz144@users.noreply.github.com>
2023-10-17 00:53:42 +0600

    Add files via upload

---
+++ limit_libaray.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "The range for short is form " << numeric_limits<short>::min() << "to" << numeric_limits<short>::max() << endl;
+    cout << "The range for short is form " << numeric_limits<unsigned short>::min() << "to" << numeric_limits<unsigned short>::max() << endl;
+
+    return 0;
+}
